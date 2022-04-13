2bc2bbe4f8afe98b8361e1b6ad389e55726db916
nikita-job <nikitka.kuznetsov.1994@mail.ru>
2022-04-13 08:16:10 +0300

    форма

--- src/App.vue
+++ src/App.vue
@@ -1,75 +1,79 @@
 <template>
-	<div class="container">
-		<div class="card p-2">
-			<h4>Актуальные новости <strong style="font-size: 14px">{{now}}</strong></h4>
-			<span>Открыто: {{openRate}} | Прочитано: {{readRate}}</span>
-		</div>
+	<div>
+		<form @submit.prevent="submit">
+			<div class="form-row">
+				<div class="col-md-4 mb-3">
+					<label for="validationServer01">First name</label>
+					<input 
+						name="name" 
+						type="text" 
+						class="form-control"
+						:class="{'is-invalid': !errors.name, 'is-valid': errors.name}" 
+						id="validationServer01" 
+						placeholder="First name"
+						v-model="name"
+					>
+					<div v-if="errors.name" class="valid-feedback">{{errors.name}}</div>
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="form-check">
+					<input class="form-check-input is-invalid" type="checkbox" value="" id="invalidCheck3" required>
+					<label class="form-check-label" for="invalidCheck3">
+						Agree to terms and conditions
+					</label>
+					<div class="invalid-feedback">
+						You must agree before submitting.
+					</div>
+				</div>
+			</div>
+			<button class="btn btn-primary">Submit form</button>
+		</form>
 	</div>
-	<AppNews 
-		v-for="item in news"
-		:key="item.id"
-		:title="item.title"
-		:id="item.id"
-		:isOpen="item.isOpen"
-		:wasRead="item.wasRead"
-		@openNews="openNews"
-		@readNews="readNews"
-		@unmark="unreadNews"
-	/>
 </template>
 
 <script>
-	import AppNews from './components/AppNews.vue'
 	export default {
 		data() {
 			return {
-				now: new Date().toLocaleDateString(),
-				isOpen: false,
-				openRate: 0,
-				readRate: 0,
-				news: [
-					{
-						title: 'Джо Байден победил на выборах в США',
-						id: 1,
-						isOpen: false,
-						wasRead: false,
-					},
-					{
-						title: 'Vue 3 работает!',
-						id: 2,
-						isOpen: false,
-						wasRead: false,
-					},
-				]
-			}
-		},
-		components: {
-			AppNews,
-		},
-		provide() {
-			return {
-				title: 'Список всех новостей',
-				news: this.news,
+				name: '',
+				errors: {
+					name: null,
+				}
 			}
 		},
 		methods: {
-			openNews() {
-				this.openRate++
+			submit() {
+				if(this.valid()) {
+					console.log('24');
+				}
 			},
-			readNews(id) {
-				const index = this.news.findIndex(news => news.id === id)
-				this.news[index].wasRead = true
-				this.readRate++
-			},
-			unreadNews(id) {
-				const news = this.news.find(news => news.id === id)
-				news.wasRead = false
-				this.readRate--
+			valid() {
+				let isValid = true
+				if(this.name !== '') {
+					isValid = false
+					this.errors.name = 'Введите имя'
+				} else {
+					this.errors.name = null
+				}
+				return isValid
 			}
 		}
 	}
 </script>
 
 <style>
+	small {
+		color: red;
+	}
 
+	select,
+	input,
+	button {
+		width: 100%;
+	}
+
+	.error {
+		border: 1px solid red;
+	}
 </style>
